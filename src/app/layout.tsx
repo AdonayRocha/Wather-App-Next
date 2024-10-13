fe6f0537485f5ca335f514279f2e28475127f732
AdonayRocha <adonay.rodrigues.rocha@gmail.com>
2024-10-13 17:30:22 -0300

    Layout - Criado
    Instalei o next 14 o que causou conflitos e foi necessário arrumar. Aparentemente, existem algumas tags que o next 14 não reconhece :(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import { ReactNode } from 'react';
 import Link from 'next/link';
 import { FaHome, FaSearch, FaSignInAlt, FaInfoCircle } from 'react-icons/fa';
@@ -7,27 +8,28 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function RootLayout({ children }: LayoutProps) {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <Link href="/" className={styles.link}>
-            <FaHome /> Home
-          </Link>
-          <Link href="/search" className={styles.link}>
-            <FaSearch /> Search
-          </Link>
-          <Link href="/login" className={styles.link}>
-            <FaSignInAlt /> Login
-          </Link>
-          <Link href="/about" className={styles.link}>
-            <FaInfoCircle /> Sobre
-          </Link>
-        </nav>
-      </header>
-      <main className={styles.content}>{children}</main>
-      <footer className={styles.footer}>Rodapé</footer>
-    </div>
+    <html lang="pt-BR">
+      <body className={styles.container}>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <Link href="/" className={styles.link}>
+              <FaHome /> Home
+            </Link>
+            <Link href="/search" className={styles.link}>
+              <FaSearch /> Search
+            </Link>
+            <Link href="/login" className={styles.link}>
+              <FaSignInAlt /> Login
+            </Link>
+            <Link href="/about" className={styles.link}>
+              <FaInfoCircle /> Sobre
+            </Link>
+          </nav>
+        </header>
+        <main className={styles.content}>{children}</main>
+      </body>
+    </html>
   );
 }
