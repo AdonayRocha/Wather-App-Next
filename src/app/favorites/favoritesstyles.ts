a206f5fb398d3ededd82018ed96c11e6e2efa2b6
Adonay Rocha <adonay.rodrigues.rocha@gmail.com>
2024-10-14 21:26:01 -0300

    Tirei o quadrado pq ficou feio

--- src/app/favorites/favoritesstyles.ts
+++ src/app/favorites/favoritesstyles.ts
@@ -4,9 +4,6 @@ export const FavoritesContainer = styled.div`
     max-width: 600px;
     margin: 0 auto;
     padding: 2em;
-    background-color: var(--background);
-    border-radius: 8px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
 export const Title = styled.h1`
--- src/app/profile/profilestyles.ts
+++ src/app/profile/profilestyles.ts
@@ -4,9 +4,6 @@ export const ProfileContainer = styled.div`
     max-width: 600px;
     margin: 0 auto;
     padding: 2em;
-    background-color: var(--background);
-    border-radius: 8px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
 export const Title = styled.h1`
